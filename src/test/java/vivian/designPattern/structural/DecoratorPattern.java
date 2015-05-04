ef129adf9a38a0e7f230f1aaffb0276d52065663
gavin.chen <543201721@qq.com>
2015-05-04 23:25:39 +0800

    装饰模式

--- src/test/java/vivian/designPattern/structural/DecoratorPattern.java
+++ src/test/java/vivian/designPattern/structural/DecoratorPattern.java
@@ -3,6 +3,20 @@ package vivian.designPattern.structural;
 /**
  * 装饰模式     结构型设计模式
  * 
+ * Decorator装饰模式是一种结构型模式，它主要是解决：“过度地使用了继承来扩展对象的功能”，由于继承为类型引入的静态特质，使得这种扩展方式缺乏灵活性；
+ * 并且随着子类的增多（扩展功能的增多），各种子类的组合（扩展功能的组合）会导致更多子类的膨胀（多继承）。
+ * 继承为类型引入的静态特质的意思是说以继承的方式使某一类型要获得功能是在编译时。所谓静态，是指在编译时；动态，是指在运行时。
+ * 
+ * GoF《设计模式》中说道：动态的给一个对象添加一些额外的职责。就增加功能而言，Decorator模式比生成子类更为灵活。
+ * 
+ * WikiPedia:
+ * In object-oriented programming, the decorator pattern (also known as Wrapper, 
+ * an alternative naming shared with the Adapter pattern) is a design pattern 
+ * that allows behavior to be added to an individual object, either statically or dynamically, 
+ * without affecting the behavior of other objects from the same class.
+ * The decorator pattern is often useful for adhering to the Single Responsibility Principle, 
+ * as it allows functionality to be divided between classes with unique areas of concern
+ * 
  * 
  * @author no1
  *
