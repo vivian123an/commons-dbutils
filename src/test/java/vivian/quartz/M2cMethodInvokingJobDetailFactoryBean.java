f8774bf33eb263f34d7dc64c552777446ad3de7e
vivian123an <543201721@qq.com>
2015-05-12 10:35:39 +0800

    test

--- src/test/java/vivian/quartz/M2cMethodInvokingJobDetailFactoryBean.java
+++ src/test/java/vivian/quartz/M2cMethodInvokingJobDetailFactoryBean.java
@@ -3,6 +3,7 @@ package vivian.quartz;
 import java.net.InetAddress;
 
 import org.apache.commons.lang.StringUtils;
+import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -10,6 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
 import org.springframework.util.MethodInvoker;
+
+import vivian.quartz.MethodInvokingJobDetailFactoryBean.MethodInvokingJob;
+import vivian.quartz.MethodInvokingJobDetailFactoryBean.StatefulMethodInvokingJob;
 
 public class M2cMethodInvokingJobDetailFactoryBean extends MethodInvokingJobDetailFactoryBean {
 	protected static final Logger logger = LoggerFactory.getLogger(M2cMethodInvokingJobDetailFactoryBean.class);
@@ -22,8 +26,14 @@ public class M2cMethodInvokingJobDetailFactoryBean extends MethodInvokingJobDeta
 
 		if (MethodInvokingJob.class.getName().equals(((JobDetail) super.getObject()).getJobClass().getName())) {
 			//((JobDetail) super.getObject()).setJobClass(M2cMethodInvokingJob.class);
+			
+			//上面是 Quartz 1.x JobDetail 的方法，Quartz 2.0 开始已经使用新的 JobBuilder
+			JobBuilder.newJob(MethodInvokingJob.class).withIdentity(super.getObject().getKey());
 		} else {
 			//((JobDetail) super.getObject()).setJobClass(M2cStatefulMethodInvokingJob.class);
+			
+			//上面是 Quartz 1.x JobDetail 的方法，Quartz 2.0 开始已经使用新的 JobBuilder
+			JobBuilder.newJob(M2cStatefulMethodInvokingJob.class).withIdentity(super.getObject().getKey());
 		}
 	}
 
